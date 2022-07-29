c4e7b99abadd41c1c7b15e85b49d6023fc47d42f
Daniel Phillips <daniel.phillips@ensono.com>
2022-07-29 13:55:16 +0100

    Increase nav timeout to try in Actions

--- src/test/java/ensono/SmartTableTest.java
+++ src/test/java/ensono/SmartTableTest.java
@@ -36,6 +36,7 @@ public class SmartTableTest {
     public void beforeEachTest() {
         context = browser.newContext();
         page = context.newPage();
+        page.setDefaultNavigationTimeout(100000);
     }
 
     @AfterEach
