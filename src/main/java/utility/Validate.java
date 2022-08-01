cca66427c9ccd0905fc3192c74d9ded0d235c58d
Daniel Phillips <daniel.phillips@ensono.com>
2022-08-01 11:21:18 +0100

    Add javadoc to validate

--- src/main/java/utility/Validate.java
+++ src/main/java/utility/Validate.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
+/**
+ * A utility class which allows for various types of validation, with results returned in an {@link ValidationResult} object
+ */
 public class Validate {
 
     private static Validate instance;
@@ -14,10 +17,26 @@ public class Validate {
         instance = this;
     }
 
+    /**
+     * Gets the singleton instance of the validator
+     * @return
+     */
     public static Validate that() {
         return instance == null ? instance = new Validate() : instance;
     }
 
+    /**
+     * Checks that all the key/values in each map are found in the other map
+     * <br>Validation is unique, e.g:
+     * <br>requiredValues =  [{k = "1", v = "1"},{k = "1", v = "1"}]
+     * <br>actualValues = [{k = "1", v = "1"}]
+     * <br>result = Fail, as only one occurrence of the key value pair was found in the second map
+     * @param requiredValues the list of maps which should be found in the second map
+     * @param actualValues the list of maps to check for matches within
+     * @param method The comparison method when checking two values {@link Method}
+     * @return {@link ValidationResult}
+     * @param <T> type of input being compared
+     */
     public <T> ValidationResult valuesArePresentInMaps(List<Map<T, T>> requiredValues, List<Map<T, T>> actualValues, Method method) {
         requiredValues = new LinkedList<>(requiredValues);
         actualValues = new LinkedList<>(actualValues);
@@ -34,6 +53,13 @@ public class Validate {
         return requiredValues.isEmpty() ? ValidationResult.pass() : ValidationResult.fail("No matches found for the following data: %s", requiredValues.toString());
     }
 
+    /**
+     * For each key in the map, applies that to the function and compares the result with the keys corresponding map value
+     * @param expectedValues Map of the expected values
+     * @param function Function to apply to each map key, where the result should be the value to compare the keys corresponding value
+     * @param method The comparison method when checking two values {@link Method}
+     * @return {@link ValidationResult}
+     */
     public ValidationResult valuesArePresentInMap(Map<String, String> expectedValues, Function<String, String> function, Method method) {
         StringBuilder builder = new StringBuilder();
         expectedValues.keySet().forEach(field -> {
@@ -43,6 +69,14 @@ public class Validate {
         return builder.toString().isBlank() ? ValidationResult.pass() : ValidationResult.fail(builder.toString());
     }
 
+    /**
+     * Checks whether all of the key/values can be found within the second map
+     * @param requiredValues Map of required values
+     * @param actualValues Map of values to check against
+     * @param method The comparison method when checking two values {@link Method}
+     * @return true if all keys/values found in the second map
+     * @param <T> input type for this method
+     */
     public <T> boolean valuesArePresentInMap(Map<T, T> requiredValues, Map<T, T> actualValues, Method method) {
         for (T key : requiredValues.keySet()) {
             if (!actualValues.containsKey(key) || compare(requiredValues.get(key), actualValues.get(key), method).failed()) {
@@ -52,6 +86,16 @@ public class Validate {
         return true;
     }
 
+    /**
+     * Checks that all required values are found in the second list
+     * <br>Validation is unique, e.g:
+     * <br>expected =  ["Eg", "Eg", "Eg"]
+     * <br>actual = ["Eg", "Eg"]
+     * <br>result = Fail, as only 2/3 occurrences of the value "Eg" was found in the second list
+     * @param expected List of values to check for
+     * @param actual List to values to check values are present inside
+     * @return {@link ValidationResult}
+     */
     public ValidationResult valuesArePresentInList(List<?> expected, List<?> actual) {
         expected = new LinkedList<>(expected);
         actual = new LinkedList<>(actual);
@@ -63,6 +107,14 @@ public class Validate {
         return expected.isEmpty() ? ValidationResult.pass() : ValidationResult.fail("The following values were not found: %s", expected.toString());
     }
 
+    /**
+     * Compares two inputs using the specified {@link Method}
+     * @param expected Expected Value
+     * @param actual Actual Value
+     * @param method {@link Method}
+     * @return {@link ValidationResult}
+     * @param <T> input type to this method
+     */
     public <T> ValidationResult compare(T expected, T actual, Method method) {
         switch (method) {
             case EQUALS:
@@ -83,6 +135,12 @@ public class Validate {
         return (String) object;
     }
 
+    /**
+     * Checks that the list is in either ascending or descending alphabetical order
+     * @param values List to check is in order
+     * @param ascending true if ascending ("A", "B", "C"), false if decending ("C", "B", "A")
+     * @return {@link ValidationResult}
+     */
     public ValidationResult listInAlphabeticalOrder(List<String> values, boolean ascending) {
         if (values.size() > 1) {
             Iterator<String> iter = values.iterator();
@@ -98,7 +156,9 @@ public class Validate {
         return ValidationResult.pass();
     }
 
-
+    /**
+     * A utility class which contains information regarding performed validation
+     */
     public static class ValidationResult {
         private final boolean status;
         private final String message;
@@ -108,32 +168,63 @@ public class Validate {
             this.message = message;
         }
 
+        /**
+         * Creates a {@link ValidationResult} object indicating validation was successful
+         * @return {@link ValidationResult}
+         */
         public static ValidationResult pass() {
             return new ValidationResult(true, null);
         }
 
+        /**
+         * Creates a {@link ValidationResult} object indicating validation failed
+         * @param message Reason validation failed
+         * @param format Any formatting to perform on the failure message
+         * @return {@link ValidationResult}
+         */
         public static ValidationResult fail(String message, String... format) {
             return new ValidationResult(false, String.format(message, format));
         }
 
+        /**
+         * Asserts that the result was a pass
+         * @return {@link ValidationResult}
+         * @throws AssertionError if validation failed
+         */
         public ValidationResult assertPass() {
             if (failed()) throw new AssertionError(message);
             return this;
         }
-
+        /**
+         * Asserts that the result was a fail
+         * @return {@link ValidationResult}
+         * @throws AssertionError if validation passed
+         */
         public ValidationResult assertFail() {
             if (passed()) throw new AssertionError("Expected a false validation result, however was true");
             return this;
         }
 
+        /**
+         * Checks if the validation passed
+         * @return true if passed
+         */
         public boolean passed() {
             return status == true;
         }
 
+        /**
+         * Checks if the validation failed
+         * @return true if failed
+         */
         public boolean failed() {
             return status == false;
         }
 
+        /**
+         * Gets the reason for failure
+         * @return String - Reason for failure
+         */
         public String getReason() {
             return message;
         }
